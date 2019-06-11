c372ca2160d485f7b8d81aab42f3a5c56655f3cf
Ovchinnikov Sergey <owchin.s@yandex.ru>
2019-06-11 15:45:36 +0300

    Возвращение к массивам, так как для вектора нужно писать конвертер

--- app/src/main/java/com/misis/brs/Database/MarkDao.java
+++ app/src/main/java/com/misis/brs/Database/MarkDao.java
@@ -5,8 +5,6 @@ import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.TypeConverters;
 
-import java.util.Vector;
-
 /**
  * список всех возможных запросов к DB для объекта Mark
  */
@@ -20,8 +18,8 @@ public interface MarkDao {
     void deleteById(long id);
 
     @Query("SELECT * FROM mark WHERE semester = :semester")
-    Vector<Mark> selectForSemester(int semester);
+    Mark[] selectForSemester(int semester);
 
     @Query("SELECT * FROM mark WHERE semester = :semester AND markType = :markType")
-    Vector<Mark> selectForSemesterAndType(int semester, @TypeConverters({Mark.class}) MarkType markType);
+    Mark[] selectForSemesterAndType(int semester, @TypeConverters({Mark.class}) MarkType markType);
 }
