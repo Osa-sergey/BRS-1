28aa5d4aa8cf03783af9bfcd1560c29c47a1a9cd
Ovchinnikov Sergey <owchin.s@yandex.ru>
2019-07-21 00:26:38 +0300

    пробую выводить новость целиком(пока что не работает)

--- app/src/main/java/com/misis/brs/Fragments/NewsFragment.java
+++ app/src/main/java/com/misis/brs/Fragments/NewsFragment.java
@@ -10,12 +10,15 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.misis.brs.Adapters.NewsViewAdapter;
 import com.misis.brs.Database.News;
+import com.misis.brs.MainActivity;
 import com.misis.brs.R;
 
 import java.util.Vector;
@@ -23,6 +26,9 @@ import java.util.Vector;
 public class NewsFragment extends Fragment {
 
     NewsViewAdapter newsViewAdapter;
+    private ListView listView;
+    private Vector<News> news;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,7 +38,7 @@ public class NewsFragment extends Fragment {
         ((TextView)getActivity().findViewById(R.id.toolbarText)).setText(R.string.news_toolbar);
         ((Spinner)getActivity().findViewById(R.id.semester_picker)).setVisibility(View.INVISIBLE);
 
-        Vector<News> news = new Vector<>();
+        news = new Vector<>();
         News item = new News();
         item.setDescription("I don't know if anybody has come across this problem but when we are in FS00 and we click on the little box beside the GL account # to look up a number we have found that if you enter an * and then number and * again in GL Long Text it will not find it. IE *703*. We have none GL number number descriptions for our internal enties but we can find them using the search tool. Anybody know why they GL Long Text does not recognize numbe7777rs? I looked in sap notes and could not find anything.");
         item.setHeader("Новость");
@@ -42,7 +48,23 @@ public class NewsFragment extends Fragment {
         news.add(item);
 
         newsViewAdapter = new NewsViewAdapter(getActivity(),news);
-        ((ListView) view.findViewById(R.id.news_list)).setAdapter(newsViewAdapter);
+        listView =(ListView) view.findViewById(R.id.news_list);
+        listView.setAdapter(newsViewAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                NewsFragment newsFragment = new NewsFragment();
+                Toast.makeText( getActivity().getApplicationContext(),"go",Toast.LENGTH_LONG).show();
+                Bundle args = new Bundle();
+                args.putString("title",news.elementAt(position).getHeader());
+                //TODO проблема со временем
+                args.putString("date",news.elementAt(position).getDateNews()+"");
+                args.putString("text",news.elementAt(position).getDescription());
+                newsFragment.setArguments(args);
+
+                ((MainActivity) getActivity()).replaceFragment(R.id.themaincontainer,newsFragment);
+            }
+        });
         return view;
     }
 
--- app/src/main/java/com/misis/brs/Fragments/NewsViewFragment.java
+++ app/src/main/java/com/misis/brs/Fragments/NewsViewFragment.java
@@ -7,15 +7,29 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.misis.brs.Database.News;
 import com.misis.brs.R;
 
 public class NewsViewFragment extends Fragment {
+    private News news;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        news.setHeader(getArguments().getString("title",""));
+        news.setDateNews(getArguments().getLong("date",0));
+        news.setDescription(getArguments().getString("text",""));
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_news_view_simple_item,container,false);
+        ((TextView) view.findViewById(R.id.title)).setText(news.getHeader());
+        ((TextView) view.findViewById(R.id.date)).setText(news.getDateNews()+"");
+        ((TextView) view.findViewById(R.id.text)).setText(news.getDescription());
 
         return view;
     }
