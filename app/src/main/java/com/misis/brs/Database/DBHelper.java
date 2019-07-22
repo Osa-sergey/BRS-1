53b44e91c7b4e5a6c3b0fd8fbdc372cc5eb584a3
Ovchinnikov Sergey <owchin.s@yandex.ru>
2019-07-22 22:57:46 +0300

    Добавлена функция проверки на возможность добавления оценки. Она нужна для того, чтобы сумма баллов не привышала 100

--- app/src/main/java/com/misis/brs/Database/DBHelper.java
+++ app/src/main/java/com/misis/brs/Database/DBHelper.java
@@ -229,4 +229,39 @@ public class DBHelper {
             Log.e("selectAllNews", e.toString());
         }return null;
     }
+
+    /**
+     * Функция проверки возможности добавления оценки для сходимости суммы
+     * @param mark добавляемая оценка
+     * @return 0 - можно добавить, 1 - лимит количества, 2 - лимит суммы баллов
+     */
+    public static int isAbleToAdd(Mark mark){
+        Mark[] ans = selectMarksForSemesterAndType(mark.getSemester(),mark.getMarkType());
+        if (ans == null) {
+            return 0;
+        }
+        switch (mark.getMarkType()){
+            case CLASS_PARTICIPATION_PART_1:
+            case CLASS_PARTICIPATION_PART_2:
+                int sum = 0;
+                for (int i = 0; i < ans.length; i++)
+                    sum += ans[i].getMark();
+                if (sum + mark.getMark() > 20)
+                    return 2;
+                else break;
+            case ONLINE_PART_1:
+            case ONLINE_PART_2:
+            case MIDTERM:
+            case FINAL_TEST:
+                if (ans.length != 0)
+                    return 1;
+                else break;
+            case PROJECT_PART_1:
+            case PROJECT_PART_2:
+                if (ans.length >= 2)
+                    return 1;
+                else break;
+        }
+        return 0;
+    }
 }
