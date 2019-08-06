1d9a4392cd420012bb447695e5a2c151a64c63f5
Ovchinnikov Sergey <owchin.s@yandex.ru>
2019-08-07 01:25:41 +0300

    Добавил макет кода для скачивания и чтения rss (надо подставить нужные параметры когда дадут ссылку на rss)

--- app/src/main/java/com/misis/brs/Fragments/NewsFragment.java
+++ app/src/main/java/com/misis/brs/Fragments/NewsFragment.java
@@ -1,5 +1,7 @@
 package com.misis.brs.Fragments;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +21,14 @@ import com.misis.brs.MainActivity;
 import com.misis.brs.R;
 import com.misis.brs.TimeHelper;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -32,9 +42,7 @@ public class NewsFragment extends Fragment {
     public void onResume() {
         super.onResume();
         //обновляем новости
-        news = getNews();
-        NewsViewAdapter.setNews(news);
-        newsViewAdapter.notifyDataSetChanged();
+        getNews();
     }
 
     @Nullable
@@ -45,8 +53,6 @@ public class NewsFragment extends Fragment {
        //изменение toolbar
         ((TextView)getActivity().findViewById(R.id.toolbarText)).setText(R.string.news_toolbar);
         ((Spinner)getActivity().findViewById(R.id.semester_picker)).setVisibility(View.INVISIBLE);
-
-        news = getNews();
 
         newsViewAdapter = new NewsViewAdapter(getActivity(),news);
         listView =(ListView) view.findViewById(R.id.news_list);
@@ -64,23 +70,87 @@ public class NewsFragment extends Fragment {
                 ((MainActivity) getActivity()).replaceFragment(R.id.themaincontainer,newsViewFragment);
             }
         });
+        //получаем новости
+        getNews();
         return view;
     }
 
-    private Vector<News> getNews() {
+    private void getNews() {
         long curDate = TimeHelper.currentTime()/1000;
 
         //удаляем новости со времени публикации которых прошло больше недели
         DBHelper.deleteNewsByDateNews(curDate-604800);
-
-        //TODO фи-я для подгрузки новостей
 
         News[] bdNews = DBHelper.selectAllNews();
         Vector<News> news = new Vector<>();
         if (bdNews != null) {
             news = new Vector<>(Arrays.asList(bdNews));
         }
-        return news;
+        //Загружаем сохранённые новости
+        NewsViewAdapter.setNews(news);
+        newsViewAdapter.notifyDataSetChanged();
+        //Скачиваем новости
+        @SuppressLint("StaticFieldLeak")
+        AsyncTask<Void,Void,Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Vector<News> news = new Vector<>();
+                try {
+                    //TODO добавить адресс
+                    URL url = new URL("");
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    //убираем namespace xml
+                    factory.setNamespaceAware(false);
+                    XmlPullParser xpp = factory.newPullParser();
+
+                    //выдаём поток данных
+                    xpp.setInput(getInputStream(url),"UTF_8");
+                    boolean insideItem = false;
+                    //тип эвента при чтении
+                    int eventType = xpp.getEventType();
+                    while(eventType != XmlPullParser.END_DOCUMENT){
+                        News newsItem = new News();
+                        if(eventType == XmlPullParser.START_TAG){
+                            if(xpp.getName().equalsIgnoreCase("item")){
+                                insideItem = true;
+                            //TODO сменить теги
+                            }else if(xpp.getName().equalsIgnoreCase("title") && insideItem){
+                                newsItem.setHeader(xpp.nextText());
+                            }else if(xpp.getName().equalsIgnoreCase("text") && insideItem){
+                                newsItem.setDescription(xpp.nextText());
+                            }else if(xpp.getName().equalsIgnoreCase("time") && insideItem){
+                                newsItem.setDateNews(Long.getLong(xpp.nextText()));
+                            }
+                            //выходим из элемента
+                        }else if(eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")){
+                            insideItem = false;
+                        }
+                        //переходим на следующий эвент
+                        eventType = xpp.next();
+                        news.add(newsItem);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+                //обновляем данные
+                NewsViewAdapter.setNews(news);
+                newsViewAdapter.notifyDataSetChanged();
+                return null;
+            }
+        };
+        //выполняем таск
+        task.execute();
+
     }
 
+    private InputStream getInputStream(URL url){
+        try {
+            return url.openConnection().getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
