87403ae39efd121a48884ed58718b88bad7b4c70
Adam <adammz@csraven.ru>
2019-08-07 14:38:20 +0300

    - Добавлены уведомления - Исправлен английский перевод snackbar

--- app/src/main/java/com/misis/brs/Fragments/HometaskAddFragment.java
+++ app/src/main/java/com/misis/brs/Fragments/HometaskAddFragment.java
@@ -1,12 +1,17 @@
 package com.misis.brs.Fragments;
 
+import android.app.AlarmManager;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +24,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.misis.brs.AlarmNotificationReceiver;
 import com.misis.brs.Database.DBHelper;
 import com.misis.brs.Database.Hometask;
 import com.misis.brs.MainActivity;
@@ -31,6 +37,7 @@ public class HometaskAddFragment extends Fragment {
 
     private long deadline;
     private Button add;
+    Hometask addingHometask;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,11 +48,11 @@ public class HometaskAddFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_edit_view_hometask,container,false);
+        View view = inflater.inflate(R.layout.fragment_add_edit_view_hometask, container, false);
 
         //изменение toolbar
-        ((TextView)getActivity().findViewById(R.id.toolbarText)).setText(TimeHelper.getTime(deadline));
-        ((Spinner)getActivity().findViewById(R.id.semester_picker)).setVisibility(View.INVISIBLE);
+        ((TextView) getActivity().findViewById(R.id.toolbarText)).setText(TimeHelper.getTime(deadline));
+        ((Spinner) getActivity().findViewById(R.id.semester_picker)).setVisibility(View.INVISIBLE);
 
         //нижнее меню
         ((MainActivity) getActivity()).emptyBottomMenu();
@@ -61,59 +68,44 @@ public class HometaskAddFragment extends Fragment {
                 InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(add.getWindowToken(), 0);
 
-                if(text.getText().toString().equals("")){
+                if (text.getText().toString().equals("")) {
                     final Snackbar notificationSnackbar = Snackbar.make(
                             v,
                             R.string.snackbarEmptyText,
                             Snackbar.LENGTH_LONG
                     );
                     notificationSnackbar.show();
-                }else {
-                    //создаём запись 
-                    Hometask addingHometask = new Hometask(deadline);
+                } else {
+                    //создаём запись
+                    addingHometask = new Hometask(deadline);
                     addingHometask.setCheckNotify(true);
                     addingHometask.setCheckDone(false);
                     addingHometask.setDescription(text.getText().toString());
 
                     SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences("Prefs", 0);
-                    addingHometask.setSemester(pref.getInt("semester",0));
+                    addingHometask.setSemester(pref.getInt("semester", 0));
                     // минус 12 часов ставим в 15:00 с учётом сдвига на +3 ( GMT+03:00)
-                    long notifyTime = addingHometask.getDeadline()-43200;
+                    long notifyTime = addingHometask.getDeadline() - 43200;
                     //отключаем нотификацию, если ее время прошло
-                    if(notifyTime < TimeHelper.currentTime()/1000){
+                    if (notifyTime < TimeHelper.currentTime() / 1000) {
                         addingHometask.setCheckNotify(false);
                     }
 
                     addingHometask.setTimeNotification(notifyTime);
                     DBHelper.insertHometask(addingHometask);
                     //TODO пока этот код не работает для больших интервалов времени
-                    if(addingHometask.getCheckNotify()){
-                        NotificationManager nm = (NotificationManager) getActivity().
-                                getApplicationContext().
-                                getSystemService(Context.NOTIFICATION_SERVICE);
 
-                        Notification.Builder builder = new Notification.Builder(getActivity().
-                                getApplicationContext());
-                        builder
-                                .setSmallIcon(R.drawable.ic_bn_tasks)
-                                .setTicker("BRS hometask deadline")
-                                .setContentTitle("Deadline")
-                                .setContentText(addingHometask.getDescription())
-                                .setWhen(addingHometask.getTimeNotification()*1000)
-                                .setAutoCancel(true);
-                        Notification notification = builder.build();
-                        nm.notify((int) addingHometask.getTimeNotification(),notification);
-                    }
+                    startNotification(false);
 
 
                     //выписываем время нотификации
                     //добавляем поддержку языков
                     String lang = Locale.getDefault().getDisplayLanguage();
                     String notify = "";
-                    if(addingHometask.getCheckNotify()) {
+                    if (addingHometask.getCheckNotify()) {
                         switch (lang) {
                             case "English":
-                                notify = "Notification will be in " + TimeHelper.getNotifTime(addingHometask.getTimeNotification());
+                                notify = "You will get the notification on " + TimeHelper.getNotifTime(addingHometask.getTimeNotification());
                                 break;
                             case "русский":
                                 notify = "Уведомление будет в " + TimeHelper.getNotifTime(addingHometask.getTimeNotification());
@@ -133,5 +125,24 @@ public class HometaskAddFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    private void startNotification(boolean isRepeating) {
+        AlarmManager manager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+        Intent intent;
+        PendingIntent pendingIntent;
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            intent = new Intent(getContext(), AlarmNotificationReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(getContext(), 0, intent, 0);
+            if (!isRepeating)
+                manager.set(AlarmManager.RTC_WAKEUP, addingHometask.getTimeNotification()*1000, pendingIntent);
+            else
+                manager.setRepeating(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime() + 3000, 86400000, pendingIntent); // 86400000 - Интервал повора 24 часа
+
+
+        }
+
     }
 }
